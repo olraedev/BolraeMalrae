cf95e59f56013eec368d8ee46c7d973a5e390e00
SSangRRae <pepe0003@kakao.com>
2024-05-09 15:38:23 +0900

    Initial Commit

---
+++ TrendingMovie/TrendingMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingMovieApp.swift
+//  TrendingMovie
+//
+//  Created by SangRae Kim on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TrendingMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
